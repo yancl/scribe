53a535b542ae27c491e6415f961ed7502c304852
yancl <kmoving@gmail.com>
2013-05-28 12:05:49 +0800

    add pip scribe

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
+import os
+import sys
+from setuptools import setup
 
-from distutils.core import setup
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r internal')
+    sys.exit()
 
 setup(
         name = "scribe_client",
-        version = "0.10",
+        version = "0.12",
         description="python scribe client for facebook scribed",
-        maintainer="yancl",
-        maintainer_email="kmoving@gmail.com",
+        long_description=open("README.md").read(),
+        author="yancl",
+        author_email="kmoving@gmail.com",
+        url='https://github.com/yancl/scribe_client',
+        classifiers=[
+            'Programming Language :: Python',
+        ],
+        platforms='Linux',
+        license='MIT License',
+        zip_safe=False,
+        install_requires=[
+            'distribute',
+            'thrift_client',
+        ],
+        tests_require=[
+            'nose',
+        ],
         packages=['scribe_client']
 )
